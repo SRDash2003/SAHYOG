b3466dcde6300168768ca5994f2914d2e0a5f88e
Soumya Ranjan Dash <165720608+SRDash2003@users.noreply.github.com>
2025-05-21 13:29:59 +0530

    Update email_config.php

--- includes/email_config.php
+++ includes/email_config.php
@@ -9,7 +9,7 @@ define('EMAIL_HOST', 'smtp.gmail.com');
 define('EMAIL_USERNAME', 'yourmail@example.com');
 define('EMAIL_PASSWORD', 'your_app_password');
 define('EMAIL_PORT', 587);
-define('EMAIL_FROM', 'soumyasanu2003@gmail.com');
+define('EMAIL_FROM', 'yourmail@example.com');
 define('EMAIL_FROM_NAME', 'SAHYOG');
 
 function getMailerInstance() {
